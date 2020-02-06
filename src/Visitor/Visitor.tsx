fdae1c2236d1618c209128be924613113b2eb472
Cristian Cordoba <cristian.cordoba@blackboard.com>
2020-02-06 17:35:20 -0500

    visitor created

---
+++ src/Visitor/Visitor.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface IVisitorProps{
+
+}
+const Visitor: React.FC<IVisitorProps> = (props: IVisitorProps) => {
+
+    return(<h1>
+        hey
+    </h1>)
+}
+
+export default Visitor;
