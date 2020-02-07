ed22e96287ca3af4513e4472815f1c9600b38cd2
Cristian Cordoba <cristian.cordoba@blackboard.com>
2020-02-06 19:18:12 -0500

    implemented filter algorithm

--- src/Server/AngelsServer.js
+++ src/Server/AngelsServer.js
@@ -1,6 +1,10 @@
 let express = require("express"); // this got me by surprise
+var cors = require('cors')
 const jsonFile = require('../Static/AGENTS_LIST.json');
 
+const corsOptions = {
+    origin: 'localhost:3000'
+  }
 
 const app = express();
 
--- src/Visitor/Visitor.tsx
+++ src/Visitor/Visitor.tsx
@@ -7,17 +7,72 @@ interface IVisitorProps{
 
 const Visitor: React.FC<IVisitorProps> = (props: IVisitorProps) => {
 
-    const [angeList, setAngelList] = useState({});
-    const [incomeValue, setIncomeValue] = useState({});
+    const [angeList, setAngelList] = useState([]);
+    const [filteredAngels, setfilteredAngels] = useState([]);
+    const [incomeValue, setIncomeValue] = useState('0');
+    const [warning, setWarning] = useState('');
+
+    const treshold = 10000;
+
+    const callForData = async () =>{
+        const list = await getAngelList();
+        console.log('api list',list);
+        setAngelList(list);
+    }
     
     useEffect(() => {
-        const list = getAngelList();
-        console.log('api list',list );
-    })
+        callForData();
+    },[])
 
-    return(<h1>
-        hey
-    </h1>)
+    const runFilterAlgorithm = () => {
+        const filteredList = angeList.filter((elem: any) =>{
+            return Math.abs( elem.income - parseInt(incomeValue) ) < treshold;
+        })
+        setfilteredAngels(filteredList);
+        console.log("filter folks", filteredList);
+    }
+
+    return(
+        <div>
+        <input 
+            placeholder="type income" 
+            onChange={(input)=>{
+                console.log("changed", input.target.value);
+                if(input.target.value.length < 5){
+                    setIncomeValue(input.target.value);
+                    setWarning('');
+                }
+                else{
+                    setWarning('you cant type more numbers');
+                }
+            }}
+            value={incomeValue}
+            type={"number"}
+        ></input> 
+        {warning.length > 0 && <h3>{warning}</h3>}
+        <button 
+            onClick={runFilterAlgorithm}
+        >
+            Match
+        </button>
+
+        <div>
+            {filteredAngels.length > 0 && 
+                filteredAngels.map((item: any) => {
+                    return(
+                        <h1 key={item.id}>{item.name}</h1>
+                    )
+                })
+            }
+        </div>
+
+       {/* {angeList.map((item: any, index) => {
+           return(
+               <h1>{item.name}</h1>
+           )
+       })} */}
+       </div>
+    )
 }
 
 export default Visitor;
