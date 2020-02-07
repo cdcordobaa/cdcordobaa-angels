39e73f33bd677d72b221bc2e4c6a033d1b711a9d
Cristian Cordoba <cristian.cordoba@blackboard.com>
2020-02-06 20:32:06 -0500

    showing the cards

--- src/Visitor/Visitor.tsx
+++ src/Visitor/Visitor.tsx
@@ -1,87 +1,79 @@
-import React, {useEffect, useState} from 'react';
-import {getAngelList} from '../Api/visitor-service';
-import AngelCard from '../Components/AngelCard/AngelCard'
+import React, { useEffect, useState } from "react";
+import { getAngelList } from "../Api/visitor-service";
+import AngelCard from "../Components/AngelCard/AngelCard";
+import "./Styles.css";
 
-interface IVisitorProps{
-
-};
+interface IVisitorProps {}
 
 const Visitor: React.FC<IVisitorProps> = (props: IVisitorProps) => {
+  const [angeList, setAngelList] = useState([]);
+  const [filteredAngels, setfilteredAngels] = useState([]);
+  const [incomeValue, setIncomeValue] = useState("0");
+  const [warning, setWarning] = useState("");
 
-    const [angeList, setAngelList] = useState([]);
-    const [filteredAngels, setfilteredAngels] = useState([]);
-    const [incomeValue, setIncomeValue] = useState('0');
-    const [warning, setWarning] = useState('');
+  const treshold = 10000;
 
-    const treshold = 10000;
+  const callForData = async () => {
+    const list = await getAngelList();
+    console.log("api list", list);
+    setAngelList(list);
+  };
 
-    const callForData = async () =>{
-        const list = await getAngelList();
-        console.log('api list',list);
-        setAngelList(list);
-    }
-    
-    useEffect(() => {
-        callForData();
-    },[])
+  useEffect(() => {
+    callForData();
+  }, []);
 
-    const runFilterAlgorithm = () => {
-        const filteredList = angeList.filter((elem: any) =>{
-            return Math.abs( elem.income - parseInt(incomeValue) ) < treshold;
-        })
-        setfilteredAngels(filteredList);
-        console.log("filter folks", filteredList);
-    }
+  const runFilterAlgorithm = () => {
+    const filteredList = angeList.filter((elem: any) => {
+      return Math.abs(elem.income - parseInt(incomeValue)) < treshold;
+    });
+    setfilteredAngels(filteredList);
+    console.log("filter folks", filteredList);
+  };
 
-    return(
-        <div>
-        <input 
-            placeholder="type income" 
-            onChange={(input)=>{
-                console.log("changed", input.target.value);
-                if(input.target.value.length < 5){
-                    setIncomeValue(input.target.value);
-                    setWarning('');
-                }
-                else{
-                    setWarning('you cant type more numbers');
-                }
-            }}
-            value={incomeValue}
-            type={"number"}
-        ></input> 
-        {warning.length > 0 && <h3>{warning}</h3>}
-        <button 
-            onClick={runFilterAlgorithm}
-        >
-            Match
-        </button>
-
-        <AngelCard
-           name="yikes"
-           id={1}
-           avatar=""
-           income={111}
-        >
-
-        </AngelCard>
-
-        <div>
-            {filteredAngels.length > 0 && 
-                filteredAngels.map((item: any) => {
-                    return(
-                        <AngelCard
-                            key={item.id}
-                            {...item}
-                        >
-
-                        </AngelCard>
-                    )
-                })
+  const matchSection = () => {
+    return (
+      <div className="match-section">
+        <input
+          placeholder="type income"
+          onChange={input => {
+            console.log("changed", input.target.value);
+            if (input.target.value.length < 5) {
+              setIncomeValue(input.target.value);
+              setWarning("");
+            } else {
+              setWarning("you cant type more numbers");
             }
-        </div>
-       </div>
-    )
-}
+          }}
+          value={incomeValue}
+          type={"number"}
+        ></input>
+        {warning.length > 0 && <h5>{warning}</h5>}
+        <button onClick={runFilterAlgorithm}>Match</button>
+      </div>
+    );
+  };
+
+  const cardSection = () => {
+    return (
+      <div className="card-row-flex">
+        {filteredAngels.length > 0 &&
+          filteredAngels.map((item: any) => {
+            return <AngelCard key={item.id} {...item}></AngelCard>;
+          })}
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      {matchSection()}
+      {(incomeValue !== '' && incomeValue !== "0") && filteredAngels.length < 1 ? 
+      <p>Please try a different income value</p> :
+      cardSection()
+      }
+    </div>
+  );
+};
 
 export default Visitor;
